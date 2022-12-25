5abb960a3dc99dc181bbffd34f78eb5accd0d50d
Heitor Giacomini <heithor_thebest@hotmail.com>
2022-12-25 20:29:08 -0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'SearchAndRescue',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44360/',
+    redirectUri: baseUrl,
+    clientId: 'SearchAndRescue_App',
+    responseType: 'code',
+    scope: 'offline_access SearchAndRescue',
+    requireHttps: true
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44360',
+      rootNamespace: 'SearchAndRescue',
+    },
+  },
+} as Environment;
